da2534aabd0bbb1803693594d97d27747a95ad68
ElsayedWaly <mosadelsayed02@gmail.com>
2025-01-29 19:35:33 +0200

    edit links

--- components/Aside.js
+++ components/Aside.js
@@ -4,6 +4,11 @@ import { IoHome } from "react-icons/io5";
 import { BsPostcard } from "react-icons/bs";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import { MdWorkOutline } from "react-icons/md";
+import { RiShoppingCartLine } from "react-icons/ri";
+import { GrGallery } from "react-icons/gr";
+import { TiContacts } from "react-icons/ti";
+import { IoSettingsOutline } from "react-icons/io5";
 
 export default function Aside() {
   const router = useRouter();
@@ -42,18 +47,18 @@ export default function Aside() {
             onClick={() => handleLinkClick("/blogs")}
           >
             <div className="flex gap-1">
-              <BsPostcard />
+              <MdWorkOutline />
               <span>Blogs</span>
             </div>
             {activeLink === "/blogs" && (
               <ul>
-                <Link href="/">
+                <Link href="/blogs">
                   <li>All Blogs</li>
                 </Link>
-                <Link href="/">
+                <Link href="/blogs/draft">
                   <li>Draft Blogs</li>
                 </Link>
-                <Link href="/">
+                <Link href="/blogs/addblog">
                   <li>Add Blogs</li>
                 </Link>
               </ul>
@@ -73,18 +78,85 @@ export default function Aside() {
             </div>
             {activeLink === "/projects" && (
               <ul>
-                <Link href="/">
+                <Link href="/projects">
                   <li>All projects</li>
                 </Link>
-                <Link href="/">
+                <Link href="/projects/draftprojects">
                   <li>Draft projects</li>
                 </Link>
-                <Link href="/">
+                <Link href="/projects/addproject">
                   <li>Add projects</li>
                 </Link>
               </ul>
             )}
           </li>
+          <li
+            className={
+              activeLink === "/shops"
+                ? "navactive flex-col flex-left"
+                : "flex-col flex-left"
+            }
+            onClick={() => handleLinkClick("/shops")}
+          >
+            <div className="flex gap-1">
+              <RiShoppingCartLine />
+              <span>Shops</span>
+            </div>
+            {activeLink === "/shops" && (
+              <ul>
+                <Link href="/shops">
+                  <li>All shops</li>
+                </Link>
+                <Link href="/shops/draftshop">
+                  <li>Draft shops</li>
+                </Link>
+                <Link href="/shops/addproduct">
+                  <li>Add shops</li>
+                </Link>
+              </ul>
+            )}
+          </li>
+          <li
+            className={
+              activeLink === "/shops"
+                ? "navactive flex-col flex-left"
+                : "flex-col flex-left"
+            }
+            onClick={() => handleLinkClick("/gallery")}
+          >
+            <div className="flex gap-1">
+              <GrGallery />
+              <span>Gallery</span>
+            </div>
+            {activeLink === "/gallery" && (
+              <ul>
+                <Link href="/gallery">
+                  <li>All gallery</li>
+                </Link>
+                <Link href="/gallery/addphoto">
+                  <li>Add gallery</li>
+                </Link>
+              </ul>
+            )}
+          </li>
+          <Link href="/contacts">
+            <li
+              className={activeLink === "/contacts" ? "navactive" : ""}
+              onClick={() => handleLinkClick("/contacts")}
+            >
+              <TiContacts />
+              <span>Contacts</span>
+            </li>
+          </Link>
+          <Link href="/setting">
+            <li
+              className={activeLink === "/setting" ? "navactive" : ""}
+              onClick={() => handleLinkClick("/setting")}
+            >
+              <IoSettingsOutline />
+              <span>setting</span>
+            </li>
+          </Link>
         </ul>
       </aside>
     </>
