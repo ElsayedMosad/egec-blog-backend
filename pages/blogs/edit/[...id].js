e29a0d83ec305803ee636554c088f4e8f4db808d
ElsayedWaly <mosadelsayed02@gmail.com>
2025-02-28 22:28:02 +0200

    start id page

--- pages/blogs/edit/[...id].js
+++ pages/blogs/edit/[...id].js
@@ -1,15 +1,26 @@
 import Blog from "@/components/Blog";
-import Head from "next/head"
+import Head from "next/head";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { BsPostcard } from "react-icons/bs";
-import { router } from 'next/router';
+import { router, useRouter } from "next/router";
 import LoginLayout from "@/components/LoginLayout";
 
 export default function EditProduct() {
+  const router = useRouter();
 
+  const { id } = router.query;
+  const [productInfo, setProductInfo] = useState(null);
 
-    return <>
-  
-    </>
+  useEffect(() => {
+    if (!id) {
+      return;
+    } else {
+      axios.get("/api/blogs?id=" + id).then((response) => {
+        setProductInfo(response.data);
+      });
+    }
+  }, [id]);
+
+  return <></>;
 }
